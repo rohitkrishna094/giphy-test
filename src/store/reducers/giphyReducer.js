b49f046d46ec861db928075a7beeecfd9b6e6554
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-11-16 21:16:56 -0500

    Added redux code

---
+++ src/store/reducers/giphyReducer.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const giphyReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default giphyReducer;
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import giphyReducer from './giphyReducer';
+
+const rootReducer = combineReducers({
+  giphy: giphyReducer
+});
+
+export default rootReducer;
