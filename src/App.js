77a6dd14056a821c9c209f7ec1134d6745594539
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-11-16 23:31:08 -0500

    Added bootstrap superhero theme

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import GifBox from './components/GifBox';
 import SearchBox from './components/SearchBox';
+import Navbar from './containers/Navbar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App mt-5">
+      <div className="App">
+        <Navbar />
         <SearchBox />
         <GifBox />
       </div>
