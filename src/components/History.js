2caf5eeb65c7f1a21ac8d8668d6239cd533ba594
rohitkrishna094 <rohitkrishna094@gmail.com>
2018-11-18 03:52:55 -0500

    Added css styles to history component

--- src/components/History.js
+++ src/components/History.js
@@ -6,7 +6,6 @@ import { deleteWord } from '../store/actions/HistoryAction';
 
 class History extends Component {
   onClick = e => {
-    console.log(e.target.innerHTML);
     this.props.getGifs(e.target.innerHTML);
   };
 
@@ -18,17 +17,37 @@ class History extends Component {
     return (
       <div>
         <h3>History</h3>
-        {this.props.links.map(link => (
-          <div className="History_cursor" key={link.id}>
-            <span className="" onClick={e => this.onClick(e)}>
-              {link.text}
-            </span>
-            <i className="ml-2 fas fa-trash-alt float-right" onClick={e => this.onDelete(e, link.id)} />
-            <br />
-          </div>
-        ))}
+        <ul className="list-group ">
+          {this.props.links.map(link => (
+            <React.Fragment key={link.id}>
+              <li className="list-group-item History_cursor">
+                <span onClick={e => this.onClick(e)}>{link.text}</span>
+                <i
+                  className="ml-2 mt-1 fas fa-trash-alt float-right text-info"
+                  onClick={e => this.onDelete(e, link.id)}
+                />
+              </li>
+              {/* <br /> */}
+            </React.Fragment>
+          ))}
+        </ul>
       </div>
     );
+
+    // return (
+    //   <div>
+    //     <h3>History</h3>
+    //     {this.props.links.map(link => (
+    //       <div className="History_cursor" key={link.id}>
+    //         <span className="" onClick={e => this.onClick(e)}>
+    //           {link.text}
+    //         </span>
+    //         <i className="ml-2 fas fa-trash-alt float-right" onClick={e => this.onDelete(e, link.id)} />
+    //         <br />
+    //       </div>
+    //     ))}
+    //   </div>
+    // );
   }
 }
 
